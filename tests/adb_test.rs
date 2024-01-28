bc393470a6043d00e023efc88eba4814ac946c11
AkiChase <1003019131@qq.com>
2024-01-28 21:24:15 +0800

    test(adb_test): add test for cmd_push

--- tests/adb_test.rs
+++ tests/adb_test.rs
@@ -1,15 +1,26 @@
 use scrcpy_client::util::Adb;
+use scrcpy_client::util::ResHelper;
 
 #[test]
 fn test_cmd_devices() {
-    let devices = Adb::cmd_devices();
-    match devices {
-        Ok(list)=>{
-            for device in &list{
-                println!("{}|{}", device.id, device.status)
-            }
-        },
-        Err(e)=>println!("{}", e)
+    let devices = Adb::cmd_devices().unwrap();
+    for device in &devices {
+        println!("{}|{}", device.id, device.status)
     }
 }
 
+#[test]
+fn test_cmd_push() {
+    let devices = Adb::cmd_devices().unwrap();
+    if devices.len() < 1 {
+        println!("no devices!")
+    } else {
+        let res = Adb::cmd_push(
+            &devices[0].id,
+            ResHelper::get_file_path("scrcpy-server").unwrap(),
+            "/data/local/tmp/scrcpy-server.jar",
+        );
+
+        println!("{}", res.unwrap());
+    }
+}
