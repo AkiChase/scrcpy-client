4fb55c0a799ab39a1c7d18e87713467d83814d74
AkiChase <1003019131@qq.com>
2024-01-29 11:18:09 +0800

    feat(adb): add shell command

--- src/util/adb.rs
+++ src/util/adb.rs
@@ -8,6 +8,64 @@ pub struct Device {
     pub status: String,
 }
 
+impl Device {
+    /// execute "adb push" to push file from src to des
+    pub fn cmd_push(&self, src: &str, des: &str) -> Result<String, AppError> {
+        let mut adb_command = Adb::cmd_base();
+        let res = adb_command
+            .args(&["-s", &self.id, "push", src, des])
+            .output();
+
+        match res {
+            Ok(output) => {
+                return Ok(String::from_utf8(output.stdout).unwrap());
+            }
+            Err(e) => {
+                return Err(AppError {
+                    type_name: "Adb".to_string(),
+                    message: e.to_string(),
+                })
+            }
+        }
+    }
+
+    /// execute "adb forward" to forward local port to the device port
+    pub fn cmd_forward(&self, local: &str, remote: &str) -> Result<(), AppError> {
+        let mut adb_command = Adb::cmd_base();
+        let res = adb_command
+            .args(&["-s", &self.id, "forward", local, remote])
+            .output();
+        if let Err(e) = res {
+            return Err(AppError {
+                type_name: "Adb".to_string(),
+                message: e.to_string(),
+            });
+        } else {
+            return Ok(());
+        }
+    }
+
+    /// execute "adb shell" to execute shell command on the device
+    pub fn cmd_shell(&self, shell_args: &[&str]) -> Result<String, AppError> {
+        let mut adb_command = Adb::cmd_base();
+        let mut args = vec!["-s", &self.id, "shell"];
+        args.extend_from_slice(shell_args);
+        let res = adb_command.args(args).output();
+
+        match res {
+            Ok(output) => {
+                return Ok(String::from_utf8(output.stdout).unwrap());
+            }
+            Err(e) => {
+                return Err(AppError {
+                    type_name: "Adb".to_string(),
+                    message: e.to_string(),
+                })
+            }
+        }
+    }
+}
+
 pub struct Adb;
 
 /// Module to execute adb command and fetch output.
@@ -17,7 +75,7 @@ impl Adb {
         Command::new(ResHelper::get_file_path("adb").unwrap())
     }
 
-    /// execute "adb devices" and return devices list or error
+    /// execute "adb devices" and return devices list
     pub fn cmd_devices() -> Result<Vec<Device>, AppError> {
         let mut adb_command = Adb::cmd_base();
         let res = adb_command.args(&["devices"]).output();
@@ -70,41 +128,6 @@ impl Adb {
     pub fn cmd_start_server() -> Result<(), AppError> {
         let mut adb_command = Adb::cmd_base();
         let res = adb_command.args(&["start-server"]).output();
-        if let Err(e) = res {
-            return Err(AppError {
-                type_name: "Adb".to_string(),
-                message: e.to_string(),
-            });
-        } else {
-            return Ok(());
-        }
-    }
-
-    /// execute "adb push" to push file from src to des
-    pub fn cmd_push(device_id: &str, src: &str, des: &str) -> Result<String, AppError> {
-        let mut adb_command = Adb::cmd_base();
-        let res = adb_command
-            .args(&["-s", device_id, "push", src, des])
-            .output();
-
-        match res {
-            Ok(output) => {
-                return Ok(String::from_utf8(output.stdout).unwrap());
-            }
-            Err(e) => {
-                return Err(AppError {
-                    type_name: "Adb".to_string(),
-                    message: e.to_string(),
-                })
-            }
-        }
-    }
-
-    pub fn cmd_forward(device_id: &str, local: &str, remote: &str) -> Result<(), AppError> {
-        let mut adb_command = Adb::cmd_base();
-        let res = adb_command
-            .args(&["-s", device_id, "forward", local, remote])
-            .output();
         if let Err(e) = res {
             return Err(AppError {
                 type_name: "Adb".to_string(),
